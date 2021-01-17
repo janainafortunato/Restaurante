13aceb75f8721d8ee8d085927e3e053778c7be32
janainafortunato <janaina.projetos@live.com>
2021-01-17 18:18:50 -0300

    Relacionamento tabela_users, tabela_cliente, tabela_funcionario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->integer('clientes_id')->unsigned();
+            $table->foreign('clientes_id')->references('id')->on('users');
+            $table->integer('funcionarios_id')->unsigned();
+            $table->foreign('funcionarios_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
